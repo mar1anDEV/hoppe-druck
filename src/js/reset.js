0278ac7c67a74cd60db16c7868808e0ffe0764c7
Marian Halibei <137928134+mar1anDEV@users.noreply.github.com>
2025-01-02 04:09:45 +0100

    Update reset.js

--- src/js/reset.js
+++ src/js/reset.js
@@ -2,6 +2,7 @@ function resetValues() {
     const resetBtn = document.getElementById('reset');
     resetBtn.addEventListener('click', function () {
         document.querySelector('.inputText').value = '';
+        document.querySelector('.inputNamen').value = '';
         document.getElementById('inputMenge').value = 1;
         console.clear();
     });
