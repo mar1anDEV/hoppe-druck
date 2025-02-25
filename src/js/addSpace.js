fe3bea0eac9e82a8e2149a5158b987cdeae22fa9
Marian Halibei <137928134+mar1anDEV@users.noreply.github.com>
2025-02-25 18:00:33 +0100

    Update addSpace.js

--- src/js/addSpace.js
+++ src/js/addSpace.js
@@ -13,6 +13,5 @@ function sanitize(string) {
 
 const userInput = '<img src="nonexistent" onerror="alert(\'XSS\')" />';
 const sanitizedInput = sanitize(userInput);
-console.log(sanitizedInput);
-// Output: '&lt;img src=&quot;nonexistent&quot; onerror=&quot;alert(&#x27;XSS&#x27;)&quot; /&gt;'
+
 
